514a6cc8fc3c1ccf516656a7e51638743b3c0d3a
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2022-01-18 19:10:05 +0100

    Creación de la clase Cita.

---
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
@@ -0,0 +1,83 @@
+package org.iesalandalus.programacion.citasclinica;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Cita {
+
+	public static final String FORMATO_FECHA_HORA = ("dd/MM/yyyy HH:mm");
+	private LocalDateTime fechaHora;
+	
+	public Paciente paciente;
+
+	//Constructor con parámetros
+	
+	public Cita(Paciente paciente, LocalDateTime fechaHora) {
+		super();
+		setFechaHora(fechaHora);
+		setPaciente(paciente);
+	}
+
+	//Constructor copia
+	
+	public Cita(Cita copiaCita)
+	{
+		if (copiaCita == null) {
+			throw new NullPointerException("ERROR: No se puede copiar una cita nula.");
+		} else {
+			setFechaHora(copiaCita.getFechaHora());
+			setPaciente(copiaCita.getPaciente());
+		}
+	}
+	
+	//Getters & Setters
+	
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+
+	public void setFechaHora(LocalDateTime fechaHora) {
+		if (fechaHora == null) {
+			throw new NullPointerException("ERROR: La fecha y hora de una cita no puede ser nula.");
+		}
+		this.fechaHora = fechaHora;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	private void setPaciente(Paciente paciente) {
+		if (paciente == null) {
+			throw new NullPointerException("ERROR: El paciente de una cita no puede ser nulo.");
+		}
+		this.paciente = new Paciente(paciente);
+	}
+
+	//Equals y hashCode
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(fechaHora);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Cita)) {
+			return false;
+		}
+		Cita other = (Cita) obj;
+		return Objects.equals(fechaHora, other.fechaHora);
+	}
+
+	//ToString
+	
+	@Override
+	public String toString() {
+		return paciente.toString() + ", fechaHora=" + fechaHora.format(DateTimeFormatter.ofPattern(FORMATO_FECHA_HORA)) + "";
+	}
+}
